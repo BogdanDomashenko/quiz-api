a9ff2e875a826add04182e0d55a24e09659cdcf6
BogdanDomashenko <got.bogdan@gmail.com>
2022-03-19 17:21:19 +0200

    upd

--- routes/quizzes.js
+++ routes/quizzes.js
@@ -8,9 +8,12 @@ router.get("/list", function (req, res, next) {
     const quizzes = [];
     files.forEach((file) => {
       const splittedFiles = file.split("/");
-      quizzes.push(
-        splittedFiles[splittedFiles.length - 1].replace(".json", "")
+      const quizzCode = splittedFiles[splittedFiles.length - 1].replace(
+        ".json",
+        ""
       );
+      const quizz = require("../quizzes/" + quizzCode);
+      quizzes.push({ code: quizzCode, type: quizz.type, name: quizz.name });
     });
     res.send(quizzes);
   });
@@ -25,6 +28,17 @@ router.get("/:quizzId", function (req, res) {
     res.status(510).send({ error_code: 510, error: "Quizz not found" });
   }
   res.send(quizz);
+});
+
+router.get("/name/:quizzId", function (req, res) {
+  let quizz;
+
+  try {
+    quizz = require("../quizzes/" + req.params.quizzId);
+  } catch (e) {
+    res.status(510).send({ error_code: 510, error: "Quizz not found" });
+  }
+  res.send({ code: req.params.quizzId, name: quizz.name });
 });
 
 module.exports = router;
